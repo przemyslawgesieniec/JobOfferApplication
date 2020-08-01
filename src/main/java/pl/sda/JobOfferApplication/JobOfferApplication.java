f166e90183244f634b6aa823dfcb6fc9781a1fb4
x_Gesiepr1 <przemyslaw.gesieniec@onespan.com>
2020-08-01 11:59:32 +0200

    Initial commit

---
+++ src/main/java/pl/sda/JobOfferApplication/JobOfferApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.JobOfferApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOfferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOfferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/JobOfferApplication/JobOfferApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.JobOfferApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOfferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
